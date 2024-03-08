8b2086d299fdb5c4070e26d2524f90e670a9d15b
SovanrothnanHam <sovanrothnan323@gmail.com>
2024-03-08 14:25:00 +0700

    Done

--- pages/Home.py
+++ pages/Home.py
@@ -4,7 +4,6 @@ def app():
    
     # Home Page Content
     st.title('Movie Review Home Page')
-
     st.subheader('Welcome to the home page of our Movie Review app!')
     # Provide information about the app
     st.write('Explore and analyze movie reviews with our app. You can build a simple classification model to understand sentiments.')
